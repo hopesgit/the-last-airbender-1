d4407e853e7b4b76e10a7025b3e5aac7c88e7e84
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-05 10:43:32 -0600

    Add information to search index view

--- spec/features/search/user_can_get_details_spec.rb
+++ spec/features/search/user_can_get_details_spec.rb
@@ -13,10 +13,22 @@ describe 'As a user,' do
       expect(current_path).to eq("/search")
       # Then I should be on page "/search"
       within('div#characters') do
-        expect(page).to have_css('div.character')
+        expect(page).to have_css('div.character', count: 97)
       end
       # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
+      within('div#characters').first(25) do
+        # not sure yet
+      end
       # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
+      within('div#characters') do
+        within('div.character'[0]) do
+          expect(page).to have_content("Name:")
+          expect(page).to have_content("Allies:")
+          expect(page).to have_content("Enemies:")
+          expect(page).to have_content("Photo:")
+          expect(page).to have_content("Affiliation:")
+        end
+      end
       # And for each of the members I should see:
       # - The name of the member (and their photo, if they have one)
       # - The list of allies or "None"
