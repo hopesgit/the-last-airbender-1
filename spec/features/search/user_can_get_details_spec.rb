e52024d53cad29dc50ef3c20efa314215b00d18f
Hope Gochnour <hope.gochnour@gmail.com>
2021-04-05 11:00:51 -0600

    Add more expectations to search index spec

--- spec/features/search/user_can_get_details_spec.rb
+++ spec/features/search/user_can_get_details_spec.rb
@@ -16,24 +16,38 @@ describe 'As a user,' do
         expect(page).to have_css('div.character', count: 97)
       end
       # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
-      within('div#characters').first(25) do
-        # not sure yet
-      end
+      # within('div#characters')[0..24] do
+      #   #expect(page).to have_content("detailed information")
+      # end
       # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
       within('div#characters') do
-        within('div.character'[0]) do
+        within(first('div.character')) do
+          first_fire = {
+            allies: [
+              "Ozai"
+            ],
+            enemies: [
+              "Earth Kingdom"
+            ],
+            name: "Chan (Fire Nation admiral)",
+            affiliation: "Fire Nation Navy"
+          }
           expect(page).to have_content("Name:")
+          expect(page).to have_content(first_fire[:name])
           expect(page).to have_content("Allies:")
+          expect(page).to have_content(first_fire[:allies][0])
           expect(page).to have_content("Enemies:")
+          expect(page).to have_content(first_fire[:enemies][0])
           expect(page).to have_content("Photo:")
           expect(page).to have_content("Affiliation:")
+          expect(page).to have_content(first_fire[:affiliation])
+          # And for each of the members I should see:
+          # - The name of the member (and their photo, if they have one)
+          # - The list of allies or "None"
+          # - The list of enemies or "None"
+          # - Any affiliations that the member has
         end
       end
-      # And for each of the members I should see:
-      # - The name of the member (and their photo, if they have one)
-      # - The list of allies or "None"
-      # - The list of enemies or "None"
-      # - Any affiliations that the member has
     end
   end
 end
